6707665ecf9d22d8639523b9835429f72475911f
Henrique Meirelles das Dores Ferraz <90532440+hmeirelles0@users.noreply.github.com>
2024-09-29 21:08:27 -0300

    script.js

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-let array = []
+let array = [];
 
 function adicionou() {
-    let n = window.document.getElementById('input')
-    let inp = Number(n.value)
+    let n = window.document.getElementById('input');
+    let inp = Number(n.value);
 
     if (inp > 100 || inp < 1) {
-        window.alert('Por favor, digite um número válido e tente novamente.')
+        window.alert('Por favor, digite um número válido e tente novamente.');
     } else {
         if (array.indexOf(inp) != -1) {
-            window.alert('Valor já encontrado na lista! Favor, digite um valor diferente.')
+            window.alert('Valor já encontrado na lista! Favor, digite um valor diferente.');
         } else {
-            array.push(inp)
-            let li = window.document.getElementById('flista')
-            let item = window.document.createElement('option')
-            item.text = `O número ${inp} foi adicionado!`
-            li.appendChild(item)
+            array.push(inp);
+            let li = window.document.getElementById('flista');
+            let item = window.document.createElement('option');
+            item.text = `O número ${inp} foi adicionado!`;
+            li.appendChild(item);
         }
     }
-    n.value = ''
-    n.focus()
-}
+    n.value = '';
+    n.focus();
+};
 
 function finalizar() {
     if (array.length == 0 || array.length == 1) {
-        window.alert('Por favor, cadastre mais números na sequência e tente novamente.')
+        window.alert('Por favor, cadastre mais números na sequência e tente novamente.');
    // } else if () {
     } else {
-        array.sort()
-        let s = Number(array[0])
+        array.sort();
+        let s = Number(array[0]);
         for (let pos = 1;pos < array.length; pos++) {
-            s += Number(array[pos]) 
-        }
+            s += Number(array[pos]);
+        };
 
-        let m = Number(array[0])
+        let m = Number(array[0]);
         for (let pos2 = 0; pos2 < array.length; pos2++) {
-            m = Number(array[pos2])
-        }
+            m = Number(array[pos2]);
+        };
         
-        let res = window.document.querySelector('div#aguardando')
+        let res = window.document.querySelector('div#aguardando');
         res.innerHTML = `Ao todo temos ${array.length} números cadastrados.
         <br><br> O menor valor cadastrado é ${array[0]}.
-        <br><br> O maior valor cadastrado é ${m}.
+        <br><br> O maior valor cadastrado é ${array[array.length - 1]}.
         <br><br> A soma dos valores é ${s}.
-        <br><br> A média dos valores é ${s/array.length}.`
-    }
+        <br><br> A média dos valores é ${s / array.length}.`;
+    };
 }
